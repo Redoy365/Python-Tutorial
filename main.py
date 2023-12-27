cdef75a646eb04a51309c5e1e7dbb13130c6c120
Redoy365 <syber.redoy.php.23.365@gmail.com>
2023-12-27 12:20:05 +0600

    formate multiple way

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-age = 24
-txt = "My name is Redoy, and I am {} year's old."
-print(txt.format(age))
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want {} pieces of item {} for {} dollars."
+print(myorder.format(quantity, itemno, price))
