694f08a0033e104e9471a8e1e25e06a8ff2fbcfa
Redoy365 <syber.redoy.php.23.365@gmail.com>
2023-12-27 12:38:24 +0600

    Python String endswith() Method

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-txt = "I love apples, apple are my favorite fruit"
+txt1 = "Hello, welcome to my world."
+txt2 = "Hello, welcome to my world!"
+txt3 = "Hello, welcome to my world?"
 
-x = txt.count("apple")
+x = txt1.endswith(".")
+y = txt2.endswith(".")
+z = txt3.endswith(".")
 
 print(x)
+print(y)
+print(z)
+
