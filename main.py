835ea0a906f563795d4c675b0b31f89ed56b099f
Redoy365 <syber.redoy.php.23.365@gmail.com>
2023-12-27 11:59:58 +0600

    Data Type

--- main.py
+++ main.py
@@ -1,11 +1,39 @@
+# Built-in Data Types
 
-x = "awesome"
-print("Python is "+x)
+x = "Hello World"
+print(type(x))
 
-def myfunc():
-    global x
-    x = "fantastic"
+x = 20
+print(type(x))
 
-myfunc()
+x = 20.5
+print(type(x))
 
-print("Python is "+x)
+x = 1j
+print(type(x))
+
+x = ["apple", "banana", "cherry"]
+print(type(x))
+
+x = range(6)
+print(type(x))
+
+x = {"name" : "John", "age" : 36}
+print(type(x))
+
+x = frozenset({"apple", "banana", "cherry"})
+print(type(x))
+
+x = True
+print(type(x))
+
+x = bytearray(5)
+print(type(x))
+
+x = memoryview(bytes(5))
+print(type(x))
+
+x = None
+print(type(x))
+
+
