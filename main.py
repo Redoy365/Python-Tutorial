15120b34e58e645733710a8ba9d9f2d38df7e8ff
Redoy365 <syber.redoy.php.23.365@gmail.com>
2023-12-27 22:31:05 +0600

    if else elif

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-mylist = ["apple", "banana", "cherry"]
-
-print(mylist)
-
-print(type(mylist))
+a = 33
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+    print("Hello")
