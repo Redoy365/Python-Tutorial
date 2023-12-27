95a7e66ff995f3240ba2ccfecf193b58d0976050
Redoy365 <syber.redoy.php.23.365@gmail.com>
2023-12-27 12:05:16 +0600

    String slicing

--- main.py
+++ main.py
@@ -1,39 +1,5 @@
-# Built-in Data Types
+x = "Hello World !"
 
-x = "Hello World"
-print(type(x))
-
-x = 20
-print(type(x))
-
-x = 20.5
-print(type(x))
-
-x = 1j
-print(type(x))
-
-x = ["apple", "banana", "cherry"]
-print(type(x))
-
-x = range(6)
-print(type(x))
-
-x = {"name" : "John", "age" : 36}
-print(type(x))
-
-x = frozenset({"apple", "banana", "cherry"})
-print(type(x))
-
-x = True
-print(type(x))
-
-x = bytearray(5)
-print(type(x))
-
-x = memoryview(bytes(5))
-print(type(x))
-
-x = None
-print(type(x))
-
+for i in range(len(x)):
+    print(x[i], end="")
 
