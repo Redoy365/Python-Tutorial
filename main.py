260e69409a4c73168ec239f909dd4262ff4bf8e5
Redoy365 <syber.redoy.php.23.365@gmail.com>
2023-12-28 11:15:45 +0600

    yield function v1.1.6

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
-class MyIterator:
-    def __init__(self, data):
-        self.data = data
-
-    def __iter__(self):
-        for item in self.data:
-            yield item
+def my_generator():
+    yield 1
+    yield 2
+    yield 3
+    yield 4
+    yield 5
 
 
 # Example usage:
-my_list = [1, 2, 3, 4, 5]
-my_iterator = MyIterator(my_list)
+gen = my_generator()
 
-for item in my_iterator:
-    print(item)
+for value in gen:
+    print(value)
