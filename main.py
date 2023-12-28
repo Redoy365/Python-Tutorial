279c048f8c54aa59cc232b5ce1ec6753621f1aeb
Redoy365 <syber.redoy.php.23.365@gmail.com>
2023-12-28 08:58:18 +0600

    Arbitrary Keyword Arguments, **kwargs Python Function v1.1.6

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-# Python Keyword Arguments
-def my_function(child3, child2, child1):
-  print("The youngest child is " + child3)
+def my_function(**kid):
+  print("His last name is " + kid["lname"])
 
-my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
+my_function(fname = "Tobias", lname = "Refsnes")
