c85f42a8b3d94e0e1464a135b736c1d6e75575a5
Redoy365 <syber.redoy.php.23.365@gmail.com>
2023-12-28 14:43:19 +0600

    class and object Advance function

--- main.py
+++ main.py
@@ -1,30 +1,27 @@
-class Car:
+class Vehicle:
   def __init__(self, brand, model):
     self.brand = brand
     self.model = model
 
   def move(self):
-    print("Drive!")
+    print("Move!")
 
-class Boat:
-  def __init__(self, brand, model):
-    self.brand = brand
-    self.model = model
+class Car(Vehicle):
+  pass
 
+class Boat(Vehicle):
   def move(self):
     print("Sail!")
 
-class Plane:
-  def __init__(self, brand, model):
-    self.brand = brand
-    self.model = model
-
+class Plane(Vehicle):
   def move(self):
     print("Fly!")
 
-car1 = Car("Ford", "Mustang")       #Create a Car class
-boat1 = Boat("Ibiza", "Touring 20") #Create a Boat class
-plane1 = Plane("Boeing", "747")     #Create a Plane class
+car1 = Car("Ford", "Mustang") #Create a Car object
+boat1 = Boat("Ibiza", "Touring 20") #Create a Boat object
+plane1 = Plane("Boeing", "747") #Create a Plane object
 
 for x in (car1, boat1, plane1):
+  print(x.brand)
+  print(x.model)
   x.move()
