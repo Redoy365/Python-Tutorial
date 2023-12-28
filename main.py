6fea227ae964de12f147c2f83dee2bad964de0b3
Redoy365 <syber.redoy.php.23.365@gmail.com>
2023-12-28 11:11:02 +0600

    next() and iter() function Object oriented system v1.1.4

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
-mytuple = ("apple", "banana", "cherry")
+class MyNumbers:
+  def __iter__(self):
+    self.a = 1
+    return self
 
-for x in mytuple:
-  print(x)
+  def __next__(self):
+    x = self.a
+    self.a += 1
+    return x
+
+myclass = MyNumbers()
+myiter = iter(myclass)
+
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
