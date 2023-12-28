a1311e8d53983d88f8e3583d73f2ce7fe6c5983f
Redoy365 <syber.redoy.php.23.365@gmail.com>
2023-12-28 10:50:27 +0600

    Python class function v1.1.1

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-# A function that returns the length of the value:
-def myFunc(e):
-  return len(e)
+class Persone:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
 
-cars = ['Ford', 'Mitsubishi', 'BMW', 'VW']
+p1 = Persone("Yusuf", 24)
+p2 = Persone("Zahirul", 29)
+p3 = Persone("Alamin", 18)
 
-cars.sort(reverse=True, key=myFunc)
-
-print(myFunc(cars))
+print("Name=",p1.name,", Age=",p1.age)
+print("Name=",p2.name,", Age=",p2.age)
+print("Name=",p3.name,", Age=",p3.age)
