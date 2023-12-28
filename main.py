9c89e45ad104a8d7af1f78827656389d07ed5c12
Redoy365 <syber.redoy.php.23.365@gmail.com>
2023-12-28 11:29:33 +0600

    Python Class and Object

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
+class Car:
+  def __init__(self, brand, model):
+    self.brand = brand
+    self.model = model
 
-print(len(thisdict))
+  def move(self):
+    print("Drive!")
+
+class Boat:
+  def __init__(self, brand, model):
+    self.brand = brand
+    self.model = model
+
+  def move(self):
+    print("Sail!")
+
+class Plane:
+  def __init__(self, brand, model):
+    self.brand = brand
+    self.model = model
+
+  def move(self):
+    print("Fly!")
+
+car1 = Car("Ford", "Mustang")       #Create a Car class
+boat1 = Boat("Ibiza", "Touring 20") #Create a Boat class
+plane1 = Plane("Boeing", "747")     #Create a Plane class
+
+for x in (car1, boat1, plane1):
+  x.move()
