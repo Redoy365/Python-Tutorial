5a5bf60a6957220f15e65161466237ed9acea979
Redoy365 <syber.redoy.php.23.365@gmail.com>
2023-12-28 10:53:53 +0600

    Python Classes and Objects v1.1.3

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ class Person:
     self.name = name
     self.age = age
 
-  def __str__(self):
-    return f"{self.name}({self.age})"
+  def myfunc(self):
+    print("Hello my name is " + self.name)
 
 p1 = Person("John", 36)
-
-print(p1)
+p1.myfunc()
