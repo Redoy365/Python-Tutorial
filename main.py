04579ce0ae0595a3a2db4952a46ab0b0baf2db9e
Redoy365 <syber.redoy.php.23.365@gmail.com>
2023-12-28 10:52:25 +0600

    class __str__ v1.1.2

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-class Persone:
+class Person:
   def __init__(self, name, age):
     self.name = name
     self.age = age
 
-p1 = Persone("Yusuf", 24)
-p2 = Persone("Zahirul", 29)
-p3 = Persone("Alamin", 18)
+  def __str__(self):
+    return f"{self.name}({self.age})"
 
-print("Name=",p1.name,", Age=",p1.age)
-print("Name=",p2.name,", Age=",p2.age)
-print("Name=",p3.name,", Age=",p3.age)
+p1 = Person("John", 36)
+
+print(p1)
