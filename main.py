d302fa153e2d228fb820e13a8a637f6702c2f2ab
Redoy365 <syber.redoy.php.23.365@gmail.com>
2023-12-28 09:44:07 +0600

    lambda function

--- main.py
+++ main.py
@@ -1,17 +1,2 @@
-def tri_recursion(k):
-  if(k > 0):
-    result = k + tri_recursion(k - 1)
-    print(result)
-  else:
-    result = 0
-  return result
-
-print("\n\nRecursion Example Results")
-tri_recursion(6)
-
-# 6+5+4+3+2+1 = 21
-# 5+4+3+2+1 = 15
-# 4+3+2+1 = 10
-# 3+2+1 = 6
-# 2+1 = 3
-# 1 = 1
+x = lambda a : a + 10
+print(x(5))
