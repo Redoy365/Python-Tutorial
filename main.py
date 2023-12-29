23f635b1218337689df09464dbbbda17fd228748
Redoy365 <syber.redoy.php.23.365@gmail.com>
2023-12-29 18:15:11 +0600

    Python format v1.1.4

--- main.py
+++ main.py
@@ -1,5 +1,2 @@
-quantity = 3
-itemno = 567
-price = 49
-myorder = "I want {0} pieces of item number {1} for {2:.2f} dollars."
-print(myorder.format(quantity, itemno, price))
+myorder = "I have a {carname}, it is a {model}."
+print(myorder.format(carname = "Ford", model = "Mustang"))
