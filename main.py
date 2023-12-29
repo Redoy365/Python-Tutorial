7a76e23498228c31996592fbe15ef072db46c72d
Redoy365 <syber.redoy.php.23.365@gmail.com>
2023-12-29 15:43:57 +0600

    Python json v1.1.3

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 import json
 
-# a Python object (dict):
-x = {
-  "name": "John",
-  "age": 30,
-  "city": "New York"
-}
-
-# convert into JSON:
-y = json.dumps(x)
-
-z = json.loads(y)
-
-# the result is a JSON string:
-print(y)
-print(z["city"])
+print(json.dumps({"name": "John", "age": 30}))
+print(json.dumps(["apple", "bananas"]))
+print(json.dumps(("apple", "bananas")))
+print(json.dumps("hello"))
+print(json.dumps(42))
+print(json.dumps(31.76))
+print(json.dumps(True))
+print(json.dumps(False))
+print(json.dumps(None))
