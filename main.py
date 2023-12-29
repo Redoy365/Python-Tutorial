0380c92f52516fada93ec0b75d341c5c6524acf5
Redoy365 <syber.redoy.php.23.365@gmail.com>
2023-12-29 18:12:53 +0600

    Python formate v1.1.2

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+quantity = 3
+itemno = 567
 price = 49
-txt = "The price is {} dollars"
-print(txt.format(price))
+myorder = "I want {} pieces of item number {} for {:.2f} dollars."
+print(myorder.format(quantity, itemno, price))
