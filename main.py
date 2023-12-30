640e8ecec945bbbb07ddb9835e2df7679a666106
Redoy365 <syber.redoy.php.23.365@gmail.com>
2023-12-30 11:27:52 +0600

    map() v1.1.5

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-# Python program to demonstrate working
-# of map.
 
-# Return double of n
-def addition(n):
-	return n + n
+def func(n):
+    return n+1
 
-# We double all numbers using map()
-numbers = (1, 2, 3, 4)
-result = map(addition, numbers)
-print(list(result))
+numbers = [1,2,3,4,5]
+x = map(func, numbers)
+
+print(list(x))
+
