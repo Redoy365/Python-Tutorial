a9b613359cb8d6e94598a1a2946fda30abe94cc9
Redoy365 <syber.redoy.php.23.365@gmail.com>
2023-12-30 11:52:38 +0600

    type()

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-set1 = {"apple", "banana", "cherry"}
-set2 = {1, 5, 7, 9, 3}
-set3 = {True, False, False}
+myset = {"apple", "banana", "cherry"}
 
-print(set1)
-print(set2)
-print(set3)
+print(type(myset))
