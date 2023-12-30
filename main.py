89fb7bd1e641a74020e8716bb3275141b81983ba
Redoy365 <syber.redoy.php.23.365@gmail.com>
2023-12-30 11:03:57 +0600

    isinstance()

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-x = int(3.5)
-y = int("3")
-z = int(float("3.6"))
+x = isinstance(5, int)
+y = isinstance(5.6, int)
+z = isinstance("4.7", int)
+
+x1 = isinstance(5.5, float)
 
 print(x)
 print(y)
 print(z)
+print(x1)
