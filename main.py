ced6dbea196a34ee2ee65a28d23bfc158e6eb908
Redoy365 <syber.redoy.php.23.365@gmail.com>
2023-12-30 15:29:03 +0600

    Python finally Keyword

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-for i in range(-5, 5):
-  if i > 0:
-    print("YES")
-  elif i == 0:
-    print("WHATEVER")
-  else:
-    print("NO")
+try:
+  x > 3
+except:
+  print("Something went wrong")
+else:
+  print("Nothing went wrong")
+finally:
+  print("The try...except block is finished")
