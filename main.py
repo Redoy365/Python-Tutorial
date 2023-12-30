f4efc776bdc16a280700255e5a539730bab9c2b3
Redoy365 <syber.redoy.php.23.365@gmail.com>
2023-12-30 11:04:52 +0600

    isinstance() v1.1.2

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-x = isinstance(5, int)
-y = isinstance(5.6, int)
-z = isinstance("4.7", int)
-
-x1 = isinstance(5.5, float)
+x = isinstance("Hello", (float, int, str, list, dict, tuple))
 
 print(x)
-print(y)
-print(z)
-print(x1)
