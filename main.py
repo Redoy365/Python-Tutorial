1a869d31ccff0b958bfc182113b5679c4944dcc6
Redoy365 <syber.redoy.php.23.365@gmail.com>
2023-12-31 21:43:18 +0600

    Python Complex

--- main.py
+++ main.py
@@ -1,22 +1,7 @@
-# Text Type:	str
-# Numeric Types:	int, float, complex
-# Sequence Types:	list, tuple, range
-# Mapping Type:	dict
-# Set Types:	set, frozenset
-# Boolean Type:	bool
-# Binary Types:	bytes, bytearray, memoryview
+x = 3+5j
+y = 5j
+z = -5j
 
-print(type("Hello"))
-print(type(3))
-print(type(3.14))
-print(type(1j))
-print(type(["apple", "banana", "cherry"]))
-print(type(("apple", "banana", "cherry")))
-print(type(range(6)))
-print(type({"name" : "John", "age" : 36}))
-print(type({"apple", "banana", "cherry"}))
-print(type(frozenset({"apple", "banana", "cherry"})))
-print(type(True))
-print(type(b"Hello"))
-print(type(bytearray(5)))
-print(type(memoryview(bytes(5))))
+print(type(x))
+print(type(y))
+print(type(z))
