76f3a6515ad109c524096d8e98fefce97b93575b
Redoy365 <syber.redoy.php.23.365@gmail.com>
2023-12-31 15:24:13 +0600

    Python Multiline Comments

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-print("Hello, World!") #This is a comment
-
 """
-Comments can be used to explain Python code.
-
-Comments can be used to make the code more readable.
-
-Comments can be used to prevent execution when testing code.
-
+This is a comment
+written in
+more than just one line
 """
+print("Hello, World!")
