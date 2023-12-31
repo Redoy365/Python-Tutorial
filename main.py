51885d0955554879719d480ca8c48c88c24457b8
Redoy365 <syber.redoy.php.23.365@gmail.com>
2023-12-31 21:49:50 +0600

    Python Multiline Strings

--- main.py
+++ main.py
@@ -1,20 +1,5 @@
-x = 1 # int
-y = 2.8 # float
-z = 1j # complex
-
-#convert from int to float:
-a = float(x)
-
-#convert from float to int:
-b = int(y)
-
-#convert from int to complex:
-c = complex(x)
-
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
 print(a)
-print(b)
-print(c)
-
-print(type(a))
-print(type(b))
-print(type(c))
