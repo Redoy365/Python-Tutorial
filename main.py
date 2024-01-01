ed3dcc7371172a1297bf524e975b5d1871287c84
Redoy365 <syber.redoy.php.23.365@gmail.com>
2024-01-01 16:49:02 +0600

    json

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import datetime
+import json
 
-x = datetime.datetime(2018, 6, 1)
+# some JSON:
+x =  '{ "name":"John", "age":30, "city":"New York"}'
 
-print(x.strftime("%B"))
+# parse x:
+y = json.loads(x)
+
+# the result is a Python dictionary:
+print(y["age"])
