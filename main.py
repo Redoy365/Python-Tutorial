a0ae9f85dcde7fe0c9777142e9dfee6eeadc41df
Redoy365 <syber.redoy.php.23.365@gmail.com>
2024-01-01 08:02:14 +0600

    Python Check In String v1.1.2

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 txt = "The rain in Spain stays mainly in the plain"
-x = "rain" in txt
-print(x)
+def myfun():
+
+    if "rain" in txt:
+        print("yes, rain in present 'txt'")
+    else:
+        print("No, rain in don't present 'txt'")
+
+myfun()
