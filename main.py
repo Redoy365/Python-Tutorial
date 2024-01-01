33c0004525be78c045c0b18494715b78e14cd9af
Redoy365 <syber.redoy.php.23.365@gmail.com>
2024-01-01 08:20:21 +0600

    Python Booleans

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-class format:
-    def __init__(self):
-        pass
-
-    def myfunc(*para):
-        myorder = "I want {2} pieces of item {0} for {1} dollars."
-        print(myorder.format(para[0], para[1], para[2]))
-
-format.myfunc(3, 567, 49.95)
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
