647ae7c29115238fb9ca6c88b598e8aaf7a143be
Redoy365 <syber.redoy.php.23.365@gmail.com>
2024-01-01 08:10:01 +0600

    Python Format String v1.1.2

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-age = 36
-txt = "My name is John, and I am {}"
-print(txt.format(age))
+class format:
+    def __init__(self):
+        pass
+
+    def myfunc(*para):
+        myorder = "I want {} pieces of item {} for {} dollars."
+        print(myorder.format(para[0], para[1], para[2]))
+
+format.myfunc(3, 567, 49, 95)
