c0d712ea8e5e98d9d4b7d151865a36ef017ca030
Redoy365 <syber.redoy.php.23.365@gmail.com>
2024-01-01 09:59:26 +0600

    Python Loop Through List Items v1.1.2

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-thislist = ["apple", "banana", "cherry"]
-for x in thislist:
-  print(x)
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+newlist = []
+
+for x in fruits:
+  if "a" in x:
+    newlist.append(x)
+
+print(newlist)
