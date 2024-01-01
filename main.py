f503fd57070dbb24bf11927925c27b0432ae6962
Redoy365 <syber.redoy.php.23.365@gmail.com>
2024-01-01 08:04:30 +0600

    Python Check In String v1.1.3 'not in'

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 txt = "The rain in Spain stays mainly in the plain"
 def myfun():
 
-    if "rain" in txt:
-        print("yes, rain in present 'txt'")
+    if "fox" not in txt:
+        print("No, fox in don't present 'txt'")
     else:
-        print("No, rain in don't present 'txt'")
+        print("yes, fox in present 'txt'")
 
 myfun()
