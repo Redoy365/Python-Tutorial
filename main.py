53fa650dbff5224fcb6b8f2399e8feb10545121d
Redoy365 <syber.redoy.php.23.365@gmail.com>
2024-01-01 09:45:48 +0600

    Python Identity Operators

--- main.py
+++ main.py
@@ -1,26 +1,5 @@
-#====
-#OR
-#====
-def OR(n):
-  print(n < 5 or n > 10)
-#====
-#AND
-#====
-def AND(n):
-  print(n < 5 and n > 10)
-#====
-#NOT
-#====
-def NOT(n):
-  print(not(n<5))
-#====
-#NAND
-#====
-def NAND(n):
-  print(not(n < 5 and n > 10))
-#====
-#NOR
-#====
-def NOR(n):
-  print(not(n < 5 or n > 10))
+x = ["apple", "banana"]
+y = ["apple", "banana"]
+z = x
 
+print(x is z)
