8f3397fe056c082f7fc9f01f15a95785fb32d891
Redoy365 <syber.redoy.php.23.365@gmail.com>
2024-01-01 16:39:23 +0600

    Python Module dir() Function

--- main.py
+++ main.py
@@ -1,17 +1,4 @@
-import platform as pf
+import platform
 
-architecture = pf.architecture()
-platform = pf.platform()
-machine = pf.machine()
-processor = pf.processor()
-version = pf.version()
-system = pf.system()
-node = pf.node()
-
-print(architecture)
-print(machine)
-print(processor)
-print(platform)
-print(version)
-print(system)
-print(node)
+x = dir(platform)
+print(x)
