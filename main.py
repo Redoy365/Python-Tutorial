ab657584f8dbea0f64a922a3a09dbe5ea9fc8f87
Redoy365 <syber.redoy.php.23.365@gmail.com>
2024-01-01 08:12:15 +0600

    Python Format String v1.1.4

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ class format:
         pass
 
     def myfunc(*para):
-        myorder = "I want {} pieces of item {} for {} dollars."
+        myorder = "I want {2} pieces of item {0} for {1} dollars."
         print(myorder.format(para[0], para[1], para[2]))
 
-format.myfunc(3, 567, 49, 95)
+format.myfunc(3, 567, 49.95)
