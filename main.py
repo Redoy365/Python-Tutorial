880d036c8c348e2f485128a40970e357b617af55
Redoy365 <syber.redoy.php.23.365@gmail.com>
2024-01-01 17:01:49 +0600

    Python Try Finally

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import re
-
-txt = "The rain in Spain "
-x = re.search("ai", txt)
-
-print(x.span()) #this will print an object
-print(x.start()) #this will print an object
-print(x.end()) #this will print an object
-print(x.group()) #this will print an object
-print(x.re) #this will print an object
-print(x.endpos) #this will print an object
+try:
+  print(x)
+except:
+  print("Something went wrong")
+finally:
+  print("The 'try except' is finished")
